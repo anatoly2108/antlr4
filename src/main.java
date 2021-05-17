0e5bfbab4d99c623e04a4b26008467a7f9b2496d
Anatoly <my_email>
2021-05-17 12:07:59 +0700

    Add base main.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,23 @@
-public class main {
-    Lexer =
+import org.antlr.v4.runtime.*;
 
+import java.io.IOException;
+
+public class main {
+
+    public static void main(String[]args) {
+        try {
+            CharStream input = CharStreams.fromFileName("./erlang/src/helloword.erl");
+            Lexer lexer = new ErlangLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ErlangParser parser = new ErlangParser(tokens);
+
+            parser.addParseListener(new MyListener());
+            parser.forms(); // корневое правило грамматики
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
+
