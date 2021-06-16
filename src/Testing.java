2521d228161021df174d771c8e7fedaca3b0d798
anatoly2108 <38525106+anatoly2108@users.noreply.github.com>
2021-06-16 20:02:41 +0700

    Add tests for messages.

--- src/Testing.java
+++ src/Testing.java
@@ -1,6 +1,7 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.Pair;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,10 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Testing {
     @Test
@@ -130,5 +133,118 @@ public class Testing {
         }
 
         assertEquals((numberOfGraphNodes - 1) * 2, numberOfPromelaChan);
+    }
+
+    @Test
+    @DisplayName("Number of messages in graph and in Promela should be equal")
+    void compareNumberOfMessages() {
+        main.FileNames fileNames = new main.FileNames();
+        int numberOfMessagesInGraph = -1;
+        int numberOfMessagesInPromela = -2;
+
+        try {
+            CharStream input = CharStreams.fromFileName(fileNames.ErlangFileName);
+            ErlangLexer lexer = new ErlangLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ErlangParser parser = new ErlangParser(tokens);
+            parser.setBuildParseTree(true);
+            ParseTree tree = parser.forms();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            MyListener listener = new MyListener();
+            walker.walk(listener, tree);
+
+            numberOfMessagesInGraph = listener.graph.edge_labels.size();
+
+            try {
+                Files.writeString(Path.of("./TesingGraph.dot"), listener.graph.toDOT(), StandardCharsets.UTF_8);
+            } catch (IOException ex) {
+                System.out.println("Error! Couldn't write TesingGraph.dot");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            CharStream input = CharStreams.fromFileName("./TesingGraph.dot");
+            DOTLexer lexer = new DOTLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            DOTParser parser = new DOTParser(tokens);
+            parser.setBuildParseTree(true);
+            ParseTree tree = parser.graph();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            MyDOTListener listener = new MyDOTListener();
+            walker.walk(listener, tree);
+
+
+            numberOfMessagesInPromela = listener.promela.edge_labels.size();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(numberOfMessagesInGraph, numberOfMessagesInPromela);
+    }
+
+    @Test
+    @DisplayName("Messages in graph and in Promela should match")
+    void compareMessages() {
+        main.FileNames fileNames = new main.FileNames();
+        boolean messagesMatch = true;
+        HashMap<Pair, String> testLabels = new HashMap<Pair, String>();
+
+        try {
+            CharStream input = CharStreams.fromFileName(fileNames.ErlangFileName);
+            ErlangLexer lexer = new ErlangLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ErlangParser parser = new ErlangParser(tokens);
+            parser.setBuildParseTree(true);
+            ParseTree tree = parser.forms();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            MyListener listener = new MyListener();
+            walker.walk(listener, tree);
+
+            testLabels = listener.graph.edge_labels;
+
+            try {
+                Files.writeString(Path.of("./TesingGraph.dot"), listener.graph.toDOT(), StandardCharsets.UTF_8);
+            } catch (IOException ex) {
+                System.out.println("Error! Couldn't write TesingGraph.dot");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            CharStream input = CharStreams.fromFileName("./TesingGraph.dot");
+            DOTLexer lexer = new DOTLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            DOTParser parser = new DOTParser(tokens);
+            parser.setBuildParseTree(true);
+            ParseTree tree = parser.graph();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            MyDOTListener listener = new MyDOTListener();
+            walker.walk(listener, tree);
+
+            if (listener.promela.edge_labels.size() == testLabels.size()) {
+                for (Pair edge: testLabels.keySet()) {
+                    if (!(listener.promela.edge_labels.containsKey(edge)
+                            && listener.promela.edge_labels.get(edge).equals(testLabels.get(edge)))) {
+                        messagesMatch = false;
+                    }
+                }
+            }
+            else {
+                messagesMatch = false;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(messagesMatch);
     }
 }
